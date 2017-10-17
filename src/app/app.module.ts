b6d77ecc5c166a28386afeb0cf7127b7477e1c3e
Oliver Haack <zccoliver@gmail.com>
2017-10-17 21:20:21 +0200

    FAB Favorites

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,6 +18,7 @@ import { DishProvider } from '../providers/dish/dish';
 import { LeaderProvider } from '../providers/leader/leader';
 import { PromotionProvider } from '../providers/promotion/promotion';
 import { ProcessHttpmsgProvider } from '../providers/process-httpmsg/process-httpmsg';
+import { FavoriteProvider } from '../providers/favorite/favorite';
 
 @NgModule({
   declarations: [
@@ -53,6 +54,7 @@ import { ProcessHttpmsgProvider } from '../providers/process-httpmsg/process-htt
     PromotionProvider,
     ProcessHttpmsgProvider,
     { provide: 'BaseURL', useValue: baseURL },
+    FavoriteProvider,
   ]
 })
 export class AppModule {}
